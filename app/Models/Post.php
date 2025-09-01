a2f95cdf4350c87c0696e98795e6a51d067c882d
FlameMeta <kulareeit@gmail.com>
2025-09-01 23:17:42 +0700

    mini cg

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,41 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-class Post
+class Post extends Model
 {
-    public static function all()
-    {
-        return [
-            [
-                'slug' => 'judul-artikel-1',
-                'title' => 'title article 1',
-                'author' => 'HIDUP (*&@^$',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio necessitatibus nihil voluptatibus quibusdam quos cum modi excepturi nobis, sint porro placeat doloribus quo eaque eligendi magni rem unde ratione quam.' 
-            ],
-            [
-                'slug' => 'judul-artikel-2',
-                'title' => 'title article 2',
-                'author' => 'mulyono',
-                'body' => 'Lorem ipsum sit amet consectetur adipisicing elit. Optio necessitatibus nihil voluptatibus quibusdam quos cum modi excepturi nobis, sint porro placeat doloribus quo eaque eligendi magni rem unde ratione quam.'
-            ]
-        ];
-    }
-
-    public static function find($slug): array # <= for make sure it return data is array 
-    {
-        // return Arr::first(static::all(), function ($post) use ($slug) {
-            //     return $post['slug'] == $slug;
-            // });
- 
-        $post = Arr::first(static::all(), fn ($post) => $post['slug'] === $slug);   //<<<=== arrow function php 8+ (callback)
-        
-        if (!$post) {
-            abort(404);
-        }
-        return $post;
-    }
 }
 
 ?>
